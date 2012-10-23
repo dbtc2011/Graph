d1b54d6326e732b9b55e6b8a055c61fb5f7471c4
DT_Macmini_Mark Angeles <dt_macmini_markangeles@DT-Macmini-Marks-Mac-mini.local>
2012-10-23 10:40:11 +0800

    Initial Commit

---
+++ Graph/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Graph
+//
+//  Created by DT_Macmini_Mark Angeles on 10/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Graph/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Graph
+//
+//  Created by DT_Macmini_Mark Angeles on 10/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
